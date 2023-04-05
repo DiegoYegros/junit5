1e495f5c210b8747b8f4ea7c7af518ade1916e26
Diego <diegoyegrosa@gmail.com>
2023-04-05 16:56:58 -0400

    revert

--- src/main/java/com/junit/junit/exceptions/DineroInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package com.junit.junit.exceptions;
-
-public class DineroInsuficienteException extends RuntimeException{
-    public DineroInsuficienteException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/junit/junit/repositories/BancoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.junit.junit.repositories;
-
-import com.junit.junit.models.Banco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BancoRepository extends JpaRepository<Banco, Long> {
-}
